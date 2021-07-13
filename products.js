26ff13cfe9323af8b01544d4234f4d30317642c9
Vikashini16 <84095993+Vikashini16@users.noreply.github.com>
2021-07-13 19:22:18 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,26 +1,4 @@
-// $(document).ready(function(){
 
-
-//     $.get("https://5fc1a1c9cb4d020016fe6b07.mockapi.io/api/v1/products",function(response){
-        
-
-//         for(var i=0;i<response.length;i++){
-//             var row = $('<tr>').addClass('table-row')
-//             var td1 = $('<td>').addClass('number').html(response[i].id)
-//             var td2 = $('<td>').html(response[i].medicineName)
-//             var td3 = $('<td>').addClass('number').html(response[i].medicineBrand)
-//             var td4 = $('<td>').html(response[i].expiryDate).addClass("date")
-//             var td5 = $('<td>').addClass('number').html(response[i].unitPrice)
-//             var td6 = $('<td>').addClass('number').html(response[i].stock)
-
-//             row.append(td1,td2,td3,td4,td5,td6)
-//             //$('#tableData').append(row)
-            
-//         }
-
-
-//     })
-// })
 
 $(document).ready(function(){
 
